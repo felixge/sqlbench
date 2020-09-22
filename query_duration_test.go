3ea365de98ba26033d87ab8a7ddd47a1d5d39ac1
Aidan Steele <aidan.steele@xero.com>
2020-09-23 06:58:25 +1000

    Test both with and without planning included

--- query_duration_test.go
+++ query_duration_test.go
@@ -9,8 +9,17 @@ func Test_queryDurationFuncs(t *testing.T) {
 	defer cleanup()
 
 	for name, fn := range queryDurationFuncs {
-		t.Run(name, func(t *testing.T) {
+		t.Run(name+" with planning", func(t *testing.T) {
 			d, err := fn(ctx, conn, "SELECT 1", true)()
+			if err != nil {
+				t.Fatal(err)
+			} else if d <= 0 {
+				t.Fatalf("bad duration: %s", d)
+			}
+		})
+
+		t.Run(name+" without planning", func(t *testing.T) {
+			d, err := fn(ctx, conn, "SELECT 1", false)()
 			if err != nil {
 				t.Fatal(err)
 			} else if d <= 0 {
